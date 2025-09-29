e2602a33ab842f15260530fa59967dbd4cff69fa
GormlessOstrich <gormlessostrich@gmail.com>
2025-09-29 13:13:17 +0100

    A-1: Implemented the basic file structure.

--- encryption.h
+++
@@ -1,8 +0,0 @@
-#ifndef ENCRYPTION_H
-#define ENCRYPTION_H
-#include <string>
-
-std::string encrypt(const std::string& password);
-std::string decrypt(const std::string& encryptedPassword);
-
-#endif
